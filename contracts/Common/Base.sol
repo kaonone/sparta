575c41aabbfb923e5924c975e7e3c371240ad45d
Alexander Mazaletskiy <alexander.mazaletskiy@gmail.com>
2019-09-05 16:09:55 +0300

    Fix initializer

--- contracts/Common/Base.sol
+++ contracts/Common/Base.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts-ethereum-package/contracts/ownership/Ownable.sol
 
 contract Base is Ownable {
 
-    function initialize(uint num) public initializer {
-     super.initialize(msg.sender);
+    function initialize() public initializer {
+       super.initialize(msg.sender);
     }
 }
