bae3a42eb68736fc9b25d3e733cfbd43a7a8c14d
Alexander Mazaletskiy <alexander.mazaletskiy@gmail.com>
2019-09-05 16:14:48 +0300

    core init

--- contracts/Common/Base.sol
+++ contracts/Common/Base.sol
@@ -2,10 +2,13 @@ pragma solidity ^0.5.0;
 
 import "@openzeppelin/contracts-ethereum-package/contracts/ownership/Ownable.sol";
 
+/*
+    Base contract for all modules
+*/
 
 contract Base is Ownable {
 
     function initialize() public initializer {
-       super.initialize(msg.sender);
+       Ownable.initialize(msg.sender);
     }
 }
---
+++ contracts/Core/Core.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.5.0;
+
+import "../Common/Base.sol";
+
+contract Core is Base {
+
+}
