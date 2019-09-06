435e34b0f6aa643269303604b8961e4f219927e3
Alexander Mazaletskiy <alexander.mazaletskiy@gmail.com>
2019-09-06 14:21:20 +0300

    Core init

--- contracts/core/Core.sol
+++ contracts/core/Core.sol
@@ -1,7 +1,35 @@
 pragma solidity ^0.5.0;
 
-import "../Common/Base.sol";
+import "../common/Base.sol";
+import "../interfaces/core/CoreInterface.sol";
 
-contract Core is Base {
+contract Core is CoreInterface, Base {
+    /* Short description */
+    string  public name;
+    string  public description;
+    address public founder;
 
+
+    /* Module constant mapping */
+    mapping(bytes32 => bool) is_constant;
+
+    /**
+     * @dev Contract ABI storage
+     *      the contract interface contains source URI
+     */
+    mapping(address => string) public abiOf;
+
+    /**
+     * @dev DAO constructor
+     * @param _name is a DAO name
+     * @param _description is a short DAO description
+     */
+
+    function initialize(string memory _name, string memory _description) public initializer {
+        name         = _name;
+        description  = _description;
+        founder      = msg.sender;
+
+        Base.initialize();
+    }
 }
