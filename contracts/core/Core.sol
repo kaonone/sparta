3fe83224c95bbb5bb372d8f2330be67daed1ce53
Alexander Mazaletskiy <alexander.mazaletskiy@gmail.com>
2019-09-09 13:24:54 +0300

    Core implementation

--- contracts/core/Core.sol
+++ contracts/core/Core.sol
@@ -40,4 +40,116 @@ contract Core is CoreInterface, Base {
         description  = _description;
         founder      = msg.sender;
     }
+
+
+    /**
+     * @dev Fast module exist check
+     * @param _module is a module address
+     * @return `true` wnen core contains module
+     */
+    function contains(address _module) public view returns (bool)
+    {
+        return modules.items.contains(_module);
+    }
+
+    /**
+     * @dev Modules counter
+     * @return count of modules in core
+     */
+    function size() public view returns (uint)
+    {
+        return modules.size();
+    }
+
+    /**
+     * @dev Check for module have permanent name
+     * @param _name is a module name
+     * @return `true` when module have permanent name
+     */
+    function isConstant(string _name) public view returns (bool)
+    {
+        return is_constant[keccak256(abi.encodePacked(_name))];
+    }
+
+    /**
+     * @dev Get module by name
+     * @param _name is module name
+     * @return module address
+     */
+    function get(string _name) public view returns (address)
+    {
+        return modules.get(_name);
+    }
+
+    /**
+     * @dev Get module name by address
+     * @param _module is a module address
+     * @return module name
+     */
+    function getName(address _module) public view returns (string)
+    {
+        return modules.keyOf[_module];
+    }
+
+    /**
+     * @dev Get first module
+     * @return first address
+     */
+    function first() public view returns (address)
+    {
+        return modules.items.head;
+    }
+
+    /**
+     * @dev Get next module
+     * @param _current is an current address
+     * @return next address
+     */
+    function next(address _current) public view returns (address)
+    {
+        return modules.items.next(_current);
+    }
+
+    /**
+     * @dev Set new module for given name
+     * @param _name infrastructure node name
+     * @param _module infrastructure node address
+     * @param _abi node interface URI
+     * @param _constant have a `true` value when you create permanent name of module
+     */
+    function set(string _name, address _module, string _abi, bool _constant) public onlyOwner {
+
+
+        require(isConstant(_name), "is not module name");
+
+        // Notify
+        if (modules.get(_name) != 0)
+            ModuleReplaced(modules.get(_name), _module);
+        else
+            ModuleAdded(_module);
+
+        // Set module in the map
+        modules.set(_name, _module);
+
+        // Register module abi
+        abiOf[_module] = _abi;
+
+        // Register constant flag
+        is_constant[sha3(_name)] = _constant;
+    }
+
+    /**
+     * @dev Remove module by name
+     * @param _name module name
+     */
+    function remove(string _name)  public onlyOwner {
+
+        require(isConstant(_name), "is not module name");
+
+        // Notify
+        ModuleRemoved(modules.get(_name));
+
+        // Remove module
+        modules.remove(_name);
+    }
 }
