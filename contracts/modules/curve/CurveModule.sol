142fb2b63d30556c1ccb007478bdef4323694710
Alexander Mazaletskiy <alexander.mazaletskiy@gmail.com>
2019-11-22 13:58:15 +0300

    bonding curve interface

---
+++ contracts/modules/curve/CurveModule.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.12;
+
+import "../../common/Base.sol";
+
+contract CurveModule is Base {
+
+    function initialize(address sender) public initializer {
+        Base.initialize(sender);
+    }
+
+    
+}
