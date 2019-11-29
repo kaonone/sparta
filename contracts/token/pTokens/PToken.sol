e65b4c7ae52b8e7dced34cb2669890b39ba6b2c7
Pavel Rubin <pash7ka@gmail.com>
2019-11-29 13:17:47 +0300

    Remove dependency on Base because PToken is not a module

--- contracts/token/pTokens/PToken.sol
+++ contracts/token/pTokens/PToken.sol
@@ -1,17 +1,18 @@
 pragma solidity ^0.5.12;
 
+import "@openzeppelin/upgrades/contracts/Initializable.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/ownership/Ownable.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Detailed.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Mintable.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Burnable.sol";
-import "../../common/Base.sol";
 
 /**
  * @notice PToken contract mock
  */
-contract PToken is Base, ERC20Detailed, ERC20Mintable, ERC20Burnable {
+contract PToken is Initializable, Ownable, ERC20Detailed, ERC20Mintable, ERC20Burnable {
 
     function initialize(address sender) public initializer {
-        Base.initialize(sender);
+        Ownable.initialize(sender);
         ERC20Detailed.initialize("pToken for DAI", "pDAI", 18);
         ERC20Mintable.initialize(sender);
     }
