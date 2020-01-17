16a64fb30ea5f46802984146e1b7f5ca5231ef71
Pavel Rubin <pash7ka@gmail.com>
2020-01-17 04:43:57 +0300

    Prettify some comments

--- contracts/common/Base.sol
+++ contracts/common/Base.sol
@@ -5,9 +5,9 @@ import "@openzeppelin/upgrades/contracts/Initializable.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/GSN/Context.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/ownership/Ownable.sol";
 
-/*
-    Base contract for all modules
-*/
+/**
+ * Base contract for all modules
+ */
 contract Base is Initializable, Context, Ownable {
     address constant  ZERO_ADDRESS = address(0);
 
