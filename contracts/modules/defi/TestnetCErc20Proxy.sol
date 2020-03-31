6c10992b1ac0aecc4ea4bef2aaf985bae144f4de
Pavel Rubin <pash7ka@gmail.com>
2020-03-31 16:05:45 +0300

    Compound integration (wip)

--- contracts/modules/defi/TestnetCErc20Proxy.sol
+++ contracts/modules/defi/TestnetCErc20Proxy.sol
@@ -2,18 +2,18 @@ pragma solidity ^0.5.12;
 
 import "../../interfaces/defi/ICErc20.sol";
 import "../../interfaces/defi/ITestnetCompoundDAI.sol";
-import "../token/FreeDAI.sol";
-import "../common/Base.sol";
+import "../../token/FreeDAI.sol";
+import "../../common/Base.sol";
 
 
 contract TestnetCErc20Proxy is ICErc20, Base {
     ITestnetCompoundDAI compoundDAI;
     FreeDAI akropolisDAI;
 
-    function initialize(address akropolisDAI, address compoundDAI) public initializer {
+    function initialize(address _akropolisDAI, address _compoundDAI) public initializer {
         Base.initialize();
-        akropolisDAI = FreeDAI(akropolisDAI);
-        compoundDAI = ITestnetCompoundDAI(compoundDAI);
+        akropolisDAI = FreeDAI(_akropolisDAI);
+        compoundDAI = ITestnetCompoundDAI(_compoundDAI);
     }
 
 }
