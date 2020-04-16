b86d5d4dc55f840a331116dc37e07a16413d611a
Pavel Rubin <pash7ka@gmail.com>
2020-04-16 17:26:50 +0300

    Fix Module test

--- test/Module.ts
+++ test/Module.ts
@@ -1,4 +1,4 @@
-import { CompoundModuleInstance, VotesModuleInstance, PoolContract, PoolInstance } from "../types/truffle-contracts/index";
+import { DefiModuleStubInstance, VotesModuleInstance, PoolContract, PoolInstance } from "../types/truffle-contracts/index";
 // tslint:disable-next-line:no-var-requires
 const { BN, constants, expectEvent, shouldFail } = require("@openzeppelin/test-helpers");
 // tslint:disable-next-line:no-var-requires
@@ -8,12 +8,12 @@ const Pool = artifacts.require("Pool");
 
 const VotesModule = artifacts.require("VotesModule");
 
-const CompoundModule = artifacts.require("CompoundModule");
+const DefiModuleStub = artifacts.require("DefiModuleStub");
 
 contract("Module", async ([_, owner, ...otherAccounts]) => {
     let pool: PoolInstance;
     let votes: VotesModuleInstance; 
-    let compound: CompoundModuleInstance;
+    let defi: DefiModuleStubInstance;
   
     beforeEach(async () => {
         pool = await Pool.new();
@@ -29,11 +29,11 @@ contract("Module", async ([_, owner, ...otherAccounts]) => {
     });
   
     it("should get next module", async () => {
-        compound = await CompoundModule.new();
-        await (<any> compound).methods['initialize(address)'](pool.address, {from: owner});
+        defi = await DefiModuleStub.new();
+        await (<any> defi).methods['initialize(address)'](pool.address, {from: owner});
         await pool.set("votes", votes.address, true, {from: owner}); 
-        await pool.set("compound", compound.address, true, {from: owner});
-        (await pool.next(votes.address)).should.equal(compound.address);
+        await pool.set("compound", defi.address, true, {from: owner});
+        (await pool.next(votes.address)).should.equal(defi.address);
     });
 
     it("should get module by name", async () => {
