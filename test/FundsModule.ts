18de1715c1ccec77dda483cdca5c773a8f6649dc
Pavel Rubin <pash7ka@gmail.com>
2020-04-21 11:32:46 +0300

    Fix funds module test

--- test/FundsModule.ts
+++ test/FundsModule.ts
@@ -33,6 +33,7 @@ contract("FundsModule", async ([_, owner, liquidityProvider, borrower, tester, .
     let pool: PoolInstance;
     let funds: FundsModuleInstance; 
     let loanm: LoanModuleStubInstance; 
+    let loanpm: LoanModuleStubInstance; 
     let curve: CurveModuleInstance; 
     let pToken: PTokenInstance;
     let lToken: FreeDAIInstance;
@@ -62,6 +63,9 @@ contract("FundsModule", async ([_, owner, liquidityProvider, borrower, tester, .
         loanm = await LoanModuleStub.new();
         await (<any> loanm).methods['initialize(address)'](pool.address, {from: owner});
         await pool.set("loan", loanm.address, true, {from: owner});  
+        loanpm = await LoanModuleStub.new();
+        await (<any> loanpm).methods['initialize(address)'](pool.address, {from: owner});
+        await pool.set("loan_proposals", loanpm.address, true, {from: owner});  
 
         funds = await FundsModule.new();
         await (<any> funds).methods['initialize(address)'](pool.address, {from: owner});
