78cbcc68d3285678630fbcbfbfeb4b34a45456e6
ilyakmet <ilyakmet@gmail.com>
2020-07-14 13:40:17 +0300

    GSNContext

--- contracts/common/Base.sol
+++ contracts/common/Base.sol
@@ -1,17 +1,17 @@
 pragma solidity ^0.5.12;
 
 import "@openzeppelin/upgrades/contracts/Initializable.sol";
-import "@openzeppelin/contracts-ethereum-package/contracts/GSN/GSNRecipient.sol";
+import "./GSNContext.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/ownership/Ownable.sol";
 
 /**
  * Base contract for all modules
  */
-contract Base is Initializable, GSNRecipient, Ownable {
+contract Base is Initializable, GSNContext, Ownable {
     address constant ZERO_ADDRESS = address(0);
 
     function initialize() public initializer {
         Ownable.initialize(_msgSender());
-        GSNRecipient.initialize();
+        GSNContext.initialize();
     }
 }
