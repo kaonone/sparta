d37eccdf20454dbdf19987de7e491ae4a50242ea
Pavel Rubin <pash7ka@gmail.com>
2020-07-24 15:14:10 +0300

    Add RAY DAI protocol to help deployment

---
+++ contracts/deploy/RAYProtocol_DAI.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.12;
+
+import "../modules/defi/RAYProtocol.sol";
+
+contract RAYProtocol_DAI is RAYProtocol {
+    bytes32 public constant PORTFOLIO_ID = keccak256("DaiCompound"); //keccak256("DaiBzxCompoundDydx")
+    function initialize(address _pool, address _token) public initializer {
+        RAYProtocol.initialize(
+            _pool, 
+            _token,
+            PORTFOLIO_ID
+        );
+    }    
+}
