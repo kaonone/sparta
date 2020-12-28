dbda37b3187f019f081d21e9a20ce498e91a1b42
Pavel Rubin <pr@akropolis.io>
2020-12-28 12:40:46 +0300

    DD-1612 added event loading [wip]

--- scripts/export_loans.js
+++ scripts/export_loans.js
@@ -4,14 +4,23 @@ const LoanProposalsModule = artifacts.require("LoanProposalsModule");
 const LoanLimitsModule = artifacts.require("LoanLimitsModule");
 
 const POOL_ADDRESS = `0x73067fdd366Cb678E9b539788F4C0f34C5700246`;
+const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 
 async function main() {
     const pool = await Pool.at(POOL_ADDRESS);
-    console.log(pool);
-    let loanModuleAddress = await pool.get.request('loan');
-    // let loanLimitsModuleAddress = await pool.get('loan_limits');
-    // let loanProposalsModuleAddress = await pool.get('loan_proposals');
+    let loanModuleAddress = await pool.get('loan', {from:ZERO_ADDRESS});
+    //let loanLimitsModuleAddress = await pool.get('loan_limits');
+    let loanProposalsModuleAddress = await pool.get('loan_proposals', {from:ZERO_ADDRESS});
     console.log('Loan module:', loanModuleAddress);
+    console.log('Loan proposals module:', loanProposalsModuleAddress);
+    const loanModule = LoanModule.at(loanModuleAddress);
+    //const loanProposalsModule = LoanProposalsModule.at(loanProposalsModuleAddress);
+    const loanProposalsModule = new web3.eth.Contract(LoanProposalsModule.abi, loanProposalsModuleAddress);
+
+    let executedProposals = await loanProposalsModule.getPastEvents('DebtProposalExecuted', { fromBlock: 0, toBlock: 'latest' });
+    console.log(executedProposals);
+
+
 
 }
 
